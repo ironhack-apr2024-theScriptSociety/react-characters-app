a174eb8e3600c0e3d6872689c1d517c74da1f8a5
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2024-05-07 14:35:23 +0200

    display list of characters

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,44 @@
+import axios from 'axios';
 import './App.css'
+import { useEffect, useState } from 'react';
+import Header from './components/Header';
 
 function App() {
 
+  const [characters, setCharacters] = useState(null);
+
+  useEffect(() => {
+    axios.get('https://ih-crud-api.herokuapp.com/characters')
+      .then( response => {
+        setCharacters(response.data);
+      })
+      .catch( e => {
+        console.log("error getting characters from the API...", e);
+      });
+  }, []);
+
+
   return (
     <>
-      <h1>React Characters App</h1>
+      
+      <Header />
+
+      { characters === null 
+        ? <h2>loading...</h2> 
+        : <h2>Number of Characters in the API: {characters.length}</h2>
+      }
+      
+
+      { characters?.map( (characterDetails, index) => {
+        return(
+          <div key={index} className='card'>
+            <p>Name: {characterDetails.name}</p>
+            <p>Occupation: {characterDetails.occupation}</p>
+            <p>Weapon: {characterDetails.weapon}</p>
+          </div>
+        );
+      })}
+
     </>
   )
 }
